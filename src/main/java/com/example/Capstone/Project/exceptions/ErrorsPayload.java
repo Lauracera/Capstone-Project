62645c853f9a0095940506da06fabae57379f444
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-03-07 12:36:52 +0100

    ErrorsPayload finish

--- src/main/java/com/example/Capstone/Project/exceptions/ErrorsPayload.java
+++ src/main/java/com/example/Capstone/Project/exceptions/ErrorsPayload.java
@@ -1,4 +1,15 @@
 package com.example.Capstone.Project.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ErrorsPayload {
+    private String message;
+    private LocalDateTime timestamp;
 }
